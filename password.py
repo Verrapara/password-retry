d94cad2a94427f4acaba41a85b0ebc74e2b4950a
Vera <xd54565852@gmail.com>
2025-09-29 18:54:38 +0800

    change to while > 0

--- password.py
+++ password.py
@@ -4,9 +4,9 @@
 # 不對的話就印出”密碼錯誤！還有＿次機會“
 # 對的話就列印出”登錄成功“
 
-password = 'a123456' #先將密碼記起來
-i = 3 #剩餘機會
-while True:
+password = 'a123456' #先將密碼記／存起來
+i = 3 #剩餘機會 變數
+while i > 0:
 	pwd = input('請輸入密碼: ') #不能存入password因為這樣重複存入，密碼縮寫是pwd
 	if pwd == password: #因為已經在password存入a123456所以不用打密碼，直接打password
 	   print('登錄成功')
@@ -14,7 +14,7 @@ while True:
 	else:
 		i = i - 1 #每輸入一次就又減一次
 		print('密碼錯誤！還有',i,'次機會') #要將字串拆解，會變成字串，正數，字串
-		if i == 0:
+		if i == 0:  #如果等於０之後就會跳出迴圈
 			break
 
 
